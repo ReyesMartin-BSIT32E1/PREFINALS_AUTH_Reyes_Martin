249ddce0bd3f0a2b648c873facacc5d52d008954
smarty0917 <martintristanmreyes@gmail.com>
2024-05-02 20:58:52 +0800

    Added ValuesController

---
+++ ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize] // Requires authentication for all actions in this controller
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("userinfo")]
+        public IActionResult GetUserInfo()
+        {
+            // Get user information from claims
+            var username = User.Identity.Name;
+
+            // Return user information
+            return Ok(new { Username = username, Section = "YourSection", Course = "YourCourse" });
+        }
+
+        [HttpGet("funfacts")]
+        public IActionResult GetFunFacts()
+        {
+            // Return fun facts about the API creator
+            string[] funFacts = {
+                "Fun fact 1",
+                "Fun fact 2",
+                // Add more fun facts here
+            };
+
+            return Ok(funFacts);
+        }
+    }
+}
