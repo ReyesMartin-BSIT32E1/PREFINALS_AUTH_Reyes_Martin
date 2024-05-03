6ba3449ffc0e6a7746bd5c9c3c152d03f4c67574
smarty0917 <martintristanmreyes@gmail.com>
2024-05-03 14:47:47 +0800

    Revert to app builder configurations

---
+++ ProtectedApi/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace ProtectedApi
+{
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Text;
+
+    namespace ProtectedApi
+    {
+        public class Startup
+        {
+            private readonly IConfiguration _configuration;
+
+            public Startup(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+
+            public void ConfigureServices(IServiceCollection services)
+            {
+                // Add JWT authentication
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = _configuration["JwtSettings:Issuer"],
+                            ValidAudience = _configuration["JwtSettings:Audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]))
+                        };
+                    });
+
+                // Other service configurations...
+            }
+
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                // Configure middleware...
+            }
+        }
+    }
+}
