05bb584ff063fc19f09ff54376d550752aeff34c
Smarty0917 <xzanderjamesnollora@gmail.com>
2024-05-04 08:30:55 +0800

    Added AuthService.cs

---
+++ AuthServer/Core/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core
+{
+    public class AuthService : IAuthService
+    {
+        public async Task<string> GenerateJwtTokenAsync(string username)
+        {
+            // Here you would implement the logic to generate a JWT token based on the provided username
+            // For demonstration purposes, let's just return a dummy token
+            string token = await Task.FromResult(Guid.NewGuid().ToString());
+            return token;
+        }
+    }
+}
--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
